e6fa5e1535f3ef6a8db20601bd95cda3b2215b1b
vaddi <sudhakarvaddi11@gmail.com>
2018-11-02 11:23:15 +0530

    Java Demo

---
+++ src/main/java/sample_java_project/App.java
@@ -0,0 +1,7 @@
+package sample_java_project;
+
+public class App {
+    public String sample() {
+    	return "Sample Java Program";
+    }
+}
---
+++ src/test/java/sample_java_project/AppTest.java
@@ -0,0 +1,13 @@
+package sample_java_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
